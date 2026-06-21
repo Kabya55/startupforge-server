d8c1d1d80695d3b575bcd5d86a9939b23aad68c2
Kabya55 <kabya@example.com>
2026-06-21 18:53:02 +0600

    update

--- index.js
+++ index.js
@@ -893,7 +893,6 @@ app.get("/api/admin/stats", verifyToken, verifyAdmin, async (req, res) => {
   });
 });
 
-// GET /api/applications/count
 // Retrieve the total count of applications in the database.
 app.get("/api/applications/count", async (req, res) => {
   try {
